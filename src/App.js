215bf8b9498d3909b5e31fdd57a32f6bd2920c7d
Gerardo Gallegos <puxerop@gmail.com>
2019-08-17 16:44:29 -0500

    Portals

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-const Computacion = () => (
-  <>
-    <li>Monitor</li>
-    <li>Mouse</li>
-    <li>Teclado</li>
-  </>
-)
+class PortalModal extends Component {
 
-const Ropa = () => (
-  <>
-    <li>Playera</li>
-    <li>Jeans</li>
-    <li>Shorts</li>
-  </>
-)
+  render () {
+
+    if (!this.props.visible) {
+      return null
+    }
+
+    const styles = {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      background: 'linear-gradient(to top right, #667eea, #764ba2',
+      opacity: '0.9',
+      color: '#FFF'
+    }
+  
+    return ReactDOM.createPortal((
+      <div style={styles}>
+        { this.props.children }
+      </div>
+    ), document.getElementById('modal-root'))
+  }
+}
 
 class App extends Component {
+  state = {
+    visible: false,
+    num: 0
+  }
+
+  componentDidMount () {
+    setInterval(() => {
+      this.setState(state => ({
+        ...state,
+        num: state.num + 1
+      }))
+    }, 1000)
+  }
+
+  mostrar = () => {
+    this.setState({ visible: true })
+  }
+
+  cerrar = () => {
+    this.setState({ visible: false })
+  }
+
   render () {
     return (
       <div>
-        <ul>
-          <Computacion />
-          <Ropa />
-        </ul>
+        <button onClick={this.mostrar}>
+          Mostrar
+        </button>
+        <PortalModal visible={this.state.visible}>
+          Hola desde un PortalModal 😲 { this.state.num }
+          <button onClick={this.cerrar}>
+            Cerrar
+          </button>
+        </PortalModal>
       </div>
     )
   }
