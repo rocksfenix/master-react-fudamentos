003aeade75636722783095b240dbcbe020422641
Gerardo Gallegos <puxerop@gmail.com>
2019-08-17 10:57:40 -0500

    Diferiencias de eventos React y html

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  state = {
-    text: '',
-    evento: ''
+
+  manejador = (e) => {
+    e.preventDefault()
+
+    console.log(e.nativeEvent)
   }
 
-  manejador = (event) => {
-    console.log(event.target.value)
-
-    this.setState({
-      text: event.target.value,
-      evento: event.type
-    })
-  }
-  
   render () {
     return (
       <div>
-        <input
-          type="text"
-          onChange={this.manejador}
-          onCopy={this.manejador}
-          onPaste={this.manejador}
-        />
-        <h1>
-          { this.state.text }
-        </h1>
-        <h2>
-          { this.state.evento }
-        </h2>
+        <a
+          href="https://google.com"
+          onClick={this.manejador}
+          // onclick="return false"
+        >
+          GOOGLE
+        </a>
       </div>
     )
   }
