8fe3ea5e32ef7f655189a81c46ac335356963a86
Gerardo Gallegos <puxerop@gmail.com>
2019-08-17 15:56:33 -0500

    Inyectando HTML

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react'
 
-const Gato = (props) => (
-  <div>
-    <h1>Gato 🐈</h1>
-    <pre>
-      { JSON.stringify(props, null, 4 ) }
-    </pre>
-  </div>
-)
-
 class App extends Component {
   state = {
-    fuerza: 100,
-    vidasRestantes: 7,
-    name: 'Chimicuil'
+    marcado: `
+      <h1>Inyectando HTML con React</h1>
+      <br/>
+      <hr/>
+      <a href="#">Algun Link</a>
+    `
   }
 
   render () {
-    const otrosDatos = {
-      raza: 'tropical',
-      peleasNocturnas: 300
-    }
     return (
       <div>
-        <Gato
-          name='Garfield'
-          age='2 años'
-          { ...otrosDatos }
-          { ...this.state }
-        />
+        <div dangerouslySetInnerHTML={{
+          __html: this.state.marcado
+        }}>
+        </div>
       </div>
     )
   }
