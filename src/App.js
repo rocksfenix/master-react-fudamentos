f2e4d7af18acb982cd07fea73bceb76c8c7ec0a8
Gerardo Gallegos <puxerop@gmail.com>
2019-08-17 16:22:35 -0500

    Fragments

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react'
 
-const Title = ({ uiColor, children }) => {
-  const styles = {
-    background: uiColor,
-    padding: '0.3em',
-    color: '#FFF',
-    borderRadius: '0.3em',
-    textAlign: 'center',
-    fontSize: '50px'
-  }
+const Computacion = () => (
+  <>
+    <li>Monitor</li>
+    <li>Mouse</li>
+    <li>Teclado</li>
+  </>
+)
 
-  return (
-    <h1 style={styles}>
-      { children }
-    </h1>
-  )
-}
-
+const Ropa = () => (
+  <>
+    <li>Playera</li>
+    <li>Jeans</li>
+    <li>Shorts</li>
+  </>
+)
 
 class App extends Component {
-  state = {
-    uiColor: 'tomato'
-  }
-
   render () {
-    const { uiColor } = this.state
     return (
       <div>
-        <Title
-          uiColor={uiColor}
-        >
-          Super <em>Ninja</em>
-        </Title>
+        <ul>
+          <Computacion />
+          <Ropa />
+        </ul>
       </div>
     )
   }
