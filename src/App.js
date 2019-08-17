ce47a3c91a586c0c670414350c4b9090f28837c8
Gerardo Gallegos <puxerop@gmail.com>
2019-08-17 16:15:12 -0500

    Introduccion a prop children

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
 
+const Title = (props) => {
+  const styles = {
+    background: props.uiColor,
+    padding: '0.3em',
+    color: '#FFF',
+    borderRadius: '0.3em',
+    textAlign: 'center',
+    fontSize: '50px'
+  }
+
+  return (
+    <h1 style={styles}>
+      { props.children }
+    </h1>
+  )
+}
+
+
 class App extends Component {
   state = {
-    marcado: `
-      <h1>Inyectando HTML con React</h1>
-      <br/>
-      <hr/>
-      <a href="#">Algun Link</a>
-    `
+    uiColor: 'purple'
   }
 
   render () {
     return (
       <div>
-        <div dangerouslySetInnerHTML={{
-          __html: this.state.marcado
-        }}>
-        </div>
+        <Title
+          uiColor={this.state.uiColor}
+        >
+          Super <em>Ninja</em>
+        </Title>
       </div>
     )
   }
